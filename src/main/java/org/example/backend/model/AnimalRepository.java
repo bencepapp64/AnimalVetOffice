6041b2f1792c8d94ccab7127feb093c55d4056eb
Norbert <szemannorbert@mailbox.unideb.hu>
2025-11-20 15:34:45 +0100

    Animal model added

---
+++ src/main/java/org/example/backend/model/AnimalRepository.java
@@ -0,0 +1,8 @@
+package org.example.backend.model;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AnimalRepository extends JpaRepository<Animal,Integer> {
+}
