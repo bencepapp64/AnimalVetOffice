08589850959408073efed2e2960619222c1e0f06
Norbert <szemannorbert@mailbox.unideb.hu>
2025-11-20 15:17:55 +0100

    MedicalEvent model added

--- src/main/java/org/example/backend/model/MedicalEvent.java
+++ src/main/java/org/example/backend/model/MedicalEvent.java
@@ -1,4 +1,25 @@
 package org.example.backend.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
 public class MedicalEvent {
+    @Id
+    @GeneratedValue
+    long id;
+    String type;
+    LocalDate date;
+    String name;
 }
